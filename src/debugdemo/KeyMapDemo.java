7d95bee92accf840ab12ede7830559c44ac57d8c
那一年的风 <842463784@qq.com>
2022-10-03 22:57:18 +0800

    add method: tryDemo

--- src/debugdemo/KeyMapDemo.java
+++ src/debugdemo/KeyMapDemo.java
@@ -1,7 +1,25 @@
 package debugdemo;
 
+import java.util.Random;
+
 public class KeyMapDemo {
     public static void printDemo(){
         System.out.println("true = " + true);
+    }
+
+    public static void tryDemo(){
+        try {
+            System.out.println("Hello from developer");
+            while (true) {
+                if (new Random().nextInt() /2 == 0) {
+                    break;
+                }
+                System.out.println("continue");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            throw new IllegalArgumentException();
+        }
     }
 }
