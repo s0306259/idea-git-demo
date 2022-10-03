f696065b4e91c79cbf80712069eb11f38b2c28fc
那一年的风 <842463784@qq.com>
2022-10-03 22:52:03 +0800

    add method

---
+++ src/debugdemo/KeyMapDemo.java
@@ -0,0 +1,7 @@
+package debugdemo;
+
+public class KeyMapDemo {
+    public static void printDemo(){
+        System.out.println("true = " + true);
+    }
+}
---
+++ src/debugdemo/TemplateDemo.java
@@ -0,0 +1,10 @@
+package debugdemo;
+
+public class TemplateDemo {
+
+    public void template1(){
+        for (int j = 0; j < 100; j++) {
+            System.out.println("j = " + j);
+        }
+    }
+}
